65f2aba0ea990e4e4da6edf6b89d8fb6fcf34ea0
Nakisani15 <nakineluheni@gmail.com>
2025-01-30 11:48:20 +0200

    commit

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+ 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
@@ -9,7 +9,9 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
+ 
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -17,8 +19,13 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+ 
+  const initialState: State = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  // [ , formAction] = useActionState(updateInvoiceWithId, initialState);
+ 
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -44,7 +51,7 @@ export default function EditInvoiceForm({
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
         </div>
-
+ 
         {/* Invoice Amount */}
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
@@ -65,7 +72,7 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </div>
-
+ 
         {/* Invoice Status */}
         <fieldset>
           <legend className="mb-2 block text-sm font-medium">
@@ -121,3 +128,4 @@ export default function EditInvoiceForm({
     </form>
   );
 }
+ 
